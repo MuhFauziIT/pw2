bf87464fb7ea4d175b1ad84db32a4926d7b92fff
MuhFauziIT <masmasfauzi.17@gmail.com>
2023-05-18 13:39:07 +0700

    tugas-10

---
+++ praktikum10/app/Http/Controllers/FormController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    // Buat Fungsi index untuk menampilkan view form
+    public function index() 
+    {
+        return view('form');
+    }
+
+
+// Buat Fungsi hasil untuk menampilkan data inputan
+    public function hasil(Request $request) 
+    {
+        $this->validate($request, [
+            'nama' => 'required',
+            'ttl' => 'required',
+            'jenis_kelamin' => 'required',
+            'hobi' => 'required',
+
+        ]);
+        // udah diisi 4 bagian
+        return view ('hasil', ['data' => $request]);
+    }
+
+
+
+}
